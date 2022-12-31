57820a3ea228a7c8b8d4c77b30b0c657cd3bf8fb
harundemirkaya <harundmrk@gmail.com>
2022-12-31 16:47:53 +0300

    Add ViewModel

---
+++ newsApp/ViewModel/NewsTableViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  NewsTableViewModel.swift
+//  newsApp
+//
+//  Created by Harun Demirkaya on 31.12.2022.
+//
+
+import Foundation
+
+struct NewsTableViewModel{
+    let newsList: [News]
+    
+    func numberOfRowsSection() -> Int {
+        return self.newsList.count
+    }
+    
+    func newsAtIndexPath(_ index: Int) -> NewsViewModel{
+        let news = self.newsList[index]
+        return NewsViewModel(news: news)
+    }
+        
+}
